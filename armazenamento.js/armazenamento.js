429fc2f9589eac15de2a9ce77415c719c70080b0
eduardo-alamino01 <eduardo.alamino09@gmail.com>
2026-03-11 08:55:17 -0300

    Commit inicial: Estrutura do projeto

---
+++ armazenamento.js/armazenamento.js
@@ -0,0 +1,5 @@
+let nome = "Carlos";
+let idade = 25;
+
+console.log("Nome: "+ nome);
+console.log("idade: " + idade);
---
+++ escopo.js/escopo.js
@@ -0,0 +1,10 @@
+function exemplo(){
+    let numero = 10;
+
+    if(numero > 5){
+        let mensagem = "o numero é maior que 5.";
+        console.log(mensagem); // Acessivel dentro do bloco if
+    }
+    console.log(numero);
+}
+exemplo();

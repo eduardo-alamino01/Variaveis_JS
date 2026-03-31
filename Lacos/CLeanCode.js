1f239d0058a132534ed588f6dbb24a17cf126da2
eduardo-alamino01 <eduardo.alamino09@gmail.com>
2026-03-31 13:47:20 -0300

    laços

---
+++ Lacos/CLeanCode.js
@@ -0,0 +1,20 @@
+/*❌ Codigo ruim
+let a = [1, 2, 3];
+let s = 0;
+for (let i = 0; i < a.length; i++) {
+  s += a[i];
+}
+console.log(s/a.length);
+*/
+
+//✅ Codigo limpo
+let notas = [1, 2, 3];
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+  soma += notas[i];
+}
+
+let media = soma / notas.length;
+
+console.log("Média:", media);
